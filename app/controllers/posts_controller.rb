6ed18a9c4186a1edbffc961b1a329a156afc97e2
越智 昭文 <akifumi.ochi@gmail.com>
2017-04-14 17:18:28 +0900

    Insta最初のコミット。devise前まで

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,51 @@
+class PostsController < ApplicationController
+  before_action :set_post, only:[:edit, :update, :destroy, :show]
+
+   def index
+     @posts = Post.all
+   end
+
+   def new
+     @post = Post.new
+   end
+
+   def create
+     @post = Post.new(posts_params)
+     if @post.save
+       redirect_to posts_path, notice: 'Posts Succeed!'
+     else
+       render 'new'
+     end
+   end
+
+   def edit
+     @post = Post.find(params[:id])
+   end
+
+   def update
+     @post = Post.find(params[:id])
+     if @post.update(posts_params)
+       redirect_to posts_path, notice: 'Posts Updated!'
+     else
+       render 'edit'
+     end
+   end
+
+   def destroy
+     @post.destroy
+     redirect_to posts_path, notice: 'Posts Deleted!'
+   end
+
+   def show
+     @post = Post.find(params[:id])
+   end
+
+private
+   def posts_params
+     params.require(:post).permit(:title, :content)
+   end
+
+   def set_post
+     @post = Post.find(params[:id])
+   end
+end
