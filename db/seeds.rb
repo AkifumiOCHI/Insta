c9442acf73d5f34351561d04b791c1d3d982cb40
越智 昭文 <akifumi.ochi@gmail.com>
2017-04-15 15:40:59 +0900

    シードデータ

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+100.times do |n|
+  email = Faker::Internet.email
+  name = "Akifumi"
+  password = "password"
+  User.create!(email: email,
+               name: name,
+               password: password,
+               password_confirmation: password,
+               )
+end
+
+n = 2
+while n <= 101
+  Post.create(
+    title: "あああ",
+    content: "いいい"
+    user_id: n
+  )
+  n = n + 1
+end
