afe8a7fbe102441ac35539c559029b12e119b6ef
越智 昭文 <akifumi.ochi@gmail.com>
2017-04-16 14:54:29 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: {
-    registrations: "users/registrations"
+    registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks"
   }
+
   resources :posts, only: [:index, :show, :create, :new, :edit, :update, :destroy,] do
   end
 
