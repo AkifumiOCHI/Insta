68352764379532f79f450758f4b3288d93518dc4
越智 昭文 <akifumi.ochi@gmail.com>
2017-04-16 15:54:41 +0900

    画像投稿できるようにimage Uploaderを追加マウント

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, :content, presence: true
   belongs_to :user
+
+  mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170416044200) do
+ActiveRecord::Schema.define(version: 20170416063019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170416044200) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.string   "image"
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,6 +47,7 @@ ActiveRecord::Schema.define(version: 20170416044200) do
     t.string   "uid",                    default: "",    null: false
     t.string   "provider",               default: "",    null: false
     t.string   "image_url"
+    t.string   "avatar"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
